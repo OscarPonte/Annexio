bbe3790d10464675f8556fda46eb390f28ef0dbf
Oscar <Oscarponteuk@gmail.com>
2020-08-27 12:38:20 +0100

    Get the list of countries from the server

--- Annexio/Controllers/CountriesController.cs
+++ Annexio/Controllers/CountriesController.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
 
 namespace Annexio.Controllers
@@ -12,24 +12,32 @@ namespace Annexio.Controllers
         // GET: Countries
         public ActionResult Index()
         {
-            var countries = new List<Country>()
-            {
-                new Country
-                {
-                    Name = "Portugal",
-                    Region = "Europe",
-                    Subregion = "Southern Europe"
-                },
-                new Country
-                {
-                    Name = "Spain",
-                    Region = "Europe",
-                    Subregion = "Southern Europe"
-                }
-            };
+            IEnumerable<Country> countries = null;
 
+            using (var client = new HttpClient())
+            {
+                var responseTask = client.GetAsync(new Uri("https://restcountries.eu/rest/v2/all"));
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IEnumerable<Country>>();
+                    readTask.Wait();
+
+                    countries = readTask.Result;
+                }
+                else
+                {
+                    countries = Enumerable.Empty<Country>();
+
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
 
             return View(countries);
         }
+
+
     }
 }
