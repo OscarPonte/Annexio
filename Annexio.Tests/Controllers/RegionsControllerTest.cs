57afa31d3c73a1527524fefa6d06de22f26c419f
Oscar <Oscarponteuk@gmail.com>
2020-08-30 16:19:18 +0100

    Unit test regions and subregions controllers

---
+++ Annexio.Tests/Controllers/RegionsControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Annexio.Controllers;
+using Annexio.Repository.Manager;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Annexio.Tests.Controllers
+{
+    public class RegionsControllerTest
+    {
+        private Mock<IRegionsManager> _mock;
+        private RegionsController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mock = new Mock<IRegionsManager>();
+            _controller = new RegionsController(_mock.Object);
+        }
+
+        [Test]
+        public void RegionsController_RegionDetails_ReturnsATaskOfViewResult()
+        {
+            var result = _controller.RegionDetails("RegionName");
+
+            Assert.IsInstanceOf<Task<ViewResult>>(result);
+        }
+
+        [Test]
+        public void RegionsController_RegionDetails_IsCallingGetRegionDetailsMethod()
+        {
+            var regionName = "RegionName";
+            var result = _controller.RegionDetails(regionName);
+
+            _mock.Verify(c => c.GetRegionDetails(regionName));
+        }
+    }
+}
