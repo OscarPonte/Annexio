aab4c8ebcd34428a2df15caadd7b7615f5ba6e24
Oscar <Oscarponteuk@gmail.com>
2020-08-30 17:38:54 +0100

    Unit Test countriuribuilder

---
+++ Annexio.Tests/Repository/UriBuilder/CountriesUriBuilderTest.cs
@@ -0,0 +1,56 @@
+﻿using Annexio.CountiresUriBuilder;
+using NUnit.Framework;
+namespace Annexio.Tests.Repository.UriBuilder
+{
+
+    public class CountriesUriBuilderTest
+    {
+        private CountriesUriBuilder _uriBuilder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uriBuilder = new CountriesUriBuilder();
+        }
+
+        [Test]
+        public void CountriesUriBuilder_GetAllCountries_ReturnsAUriToGetAllCountries()
+        {
+            var result = _uriBuilder.GetAllCountries().ToString();
+
+            Assert.That(result, Is.EqualTo("https://restcountries.eu/rest/v2/all"));
+        }
+
+        [Test]
+        public void CountriesUriBuilder_GetCountryByName_ReturnsAUriToGetCountryDetailsByName()
+        {
+            var result = _uriBuilder.GetCountryByName("Name").ToString();
+
+            Assert.That(result, Is.EqualTo("https://restcountries.eu/rest/v2/name/Name"));
+        }
+
+        [Test]
+        public void CountriesUriBuilder_GetCountryByCode_ReturnsAUriToGetCountryDetailsByCode()
+        {
+            var result = _uriBuilder.GetCountryByCode("Code").ToString();
+
+            Assert.That(result, Is.EqualTo("https://restcountries.eu/rest/v2/alpha/Code"));
+        }
+
+        [Test]
+        public void CountriesUriBuilder_GetRegion_ReturnsAUriToGetAllCountriesByRegion()
+        {
+            var result = _uriBuilder.GetRegion("Region").ToString();
+
+            Assert.That(result, Is.EqualTo("https://restcountries.eu/rest/v2/region/Region"));
+        }
+        [Test]
+        public void CountriesUriBuilder_GetSubregion_ReturnsAUriToGetAllCountriesBySubregion()
+        {
+            var result = _uriBuilder.GetSubregion("Subregion").ToString();
+
+            Assert.That(result, Is.EqualTo("https://restcountries.eu/rest/v2/subregion/Subregion"));
+        }
+
+    }
+}
